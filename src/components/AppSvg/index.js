ea40d0e7b1fdf8f23b8541b508944c226461e88c
dqphuong2k2 <dqphuong2k2@gmail.com>
2023-10-01 22:37:28 +0700

    update AppSvg

--- src/components/AppSvg/index.js
+++ src/components/AppSvg/index.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {SvgXml} from 'react-native-svg';
+import {View} from 'react-native';
 
 AppSvg.propTypes = {
   svgSrc: PropTypes.any,
   size: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  style: PropTypes.object,
 };
 
 AppSvg.defaultProps = {
   svgSrc: null,
   size: 24,
+  style: {},
 };
 
 function AppSvg(props) {
-  const {svgSrc, size} = props;
+  const {svgSrc, size, style} = props;
 
-  return <SvgXml height={size} width={size} xml={svgSrc} />;
+  return (
+    <View
+      style={{
+        aspectRatio: 1,
+        height: size,
+        width: undefined,
+        justifyContent: 'center',
+        alignItems: 'center',
+        ...style,
+      }}>
+      <SvgXml height={'100%'} width={'100%'} xml={svgSrc} />
+    </View>
+  );
 }
 
 export default AppSvg;
