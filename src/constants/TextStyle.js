891839b66be030d4f6c1d66a762d4366ec453d39
dqphuong2k2 <dqphuong2k2@gmail.com>
2023-10-03 15:28:18 +0700

    update color + typography

---
+++ src/constants/TextStyle.js
@@ -0,0 +1,182 @@
+import AppFonts from './AppFonts';
+
+const TextStyle = {
+  title: {
+    large: {
+      fontFamily: AppFonts.medium,
+      fontWeight: '500',
+      fontSize: 38,
+      lineHeight: 48,
+    },
+    t_1: {
+      fontFamily: AppFonts.medium,
+      fontWeight: '500',
+      fontSize: 24,
+      lineHeight: 32,
+    },
+    t_2: {
+      fontFamily: AppFonts.medium,
+      fontWeight: '500',
+      fontSize: 20,
+      lineHeight: 28,
+    },
+    t_3: {
+      fontFamily: AppFonts.medium,
+      fontWeight: '500',
+      fontSize: 16,
+      lineHeight: 24,
+    },
+    t_4: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 16,
+      lineHeight: 22,
+    },
+    t_5: {
+      fontFamily: AppFonts.medium,
+      fontWeight: '500',
+      fontSize: 14,
+      lineHeight: 22,
+    },
+    t_6: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 14,
+      lineHeight: 22,
+    },
+  },
+  body: {
+    b_1: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 16,
+      lineHeight: 28,
+    },
+    b_2: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 14,
+      lineHeight: 22,
+    },
+  },
+  button: {
+    btn_1: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '500',
+      fontSize: 16,
+      lineHeight: 24,
+    },
+    btn_2: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 14,
+      lineHeight: 22,
+    },
+    btn_3: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 12,
+      lineHeight: 16,
+    },
+  },
+  subtitle: {
+    s_1: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 16,
+      lineHeight: 24,
+    },
+    s_2: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 14,
+      lineHeight: 22,
+    },
+    s_3: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 12,
+      lineHeight: 16,
+    },
+    s_4: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 10,
+      lineHeight: 14,
+    },
+  },
+  label: {
+    l_1: {
+      fontFamily: AppFonts.medium,
+      fontWeight: '500',
+      fontSize: 16,
+      lineHeight: 24,
+    },
+    l_2: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 16,
+      lineHeight: 22,
+    },
+    l_3: {
+      fontFamily: AppFonts.medium,
+      fontWeight: '500',
+      fontSize: 14,
+      lineHeight: 22,
+    },
+    l_4: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 14,
+      lineHeight: 22,
+    },
+    l_5: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 12,
+      lineHeight: 16,
+    },
+  },
+  placeholder: {
+    p_1: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 16,
+      lineHeight: 28,
+    },
+    p_2: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 14,
+      lineHeight: 22,
+    },
+  },
+  caption: {
+    c_1: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 14,
+      lineHeight: 22,
+    },
+    c_2: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 12,
+      lineHeight: 16,
+    },
+    c_3: {
+      fontFamily: AppFonts.regular,
+      fontWeight: '400',
+      fontSize: 10,
+      lineHeight: 14,
+    },
+  },
+  footnote: {
+    fontFamily: AppFonts.regular,
+    fontWeight: '400',
+    fontSize: 12,
+    lineHeight: 16,
+  },
+};
+
+export default TextStyle;
